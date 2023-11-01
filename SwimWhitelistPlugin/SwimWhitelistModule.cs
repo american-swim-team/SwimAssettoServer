2f0c1736c07dc15b537c43d8ed52c113f46f722a
Rom3dius <rom3dius@yahoo.com>
2023-11-01 17:24:26 +0100

    workflow updates, remove unncessary plugins, auto register projects, added SwimWhitelistPlugin

---
+++ SwimWhitelistPlugin/SwimWhitelistModule.cs
@@ -0,0 +1,13 @@
+﻿using AssettoServer.Server.OpenSlotFilters;
+using AssettoServer.Server.Plugin;
+using Autofac;
+
+namespace SwimWhitelistPlugin;
+
+public class SwimWhitelistModule : AssettoServerModule<SwimWhitelistConfiguration>
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<SwimWhitelistFilter>().As<IOpenSlotFilter>().SingleInstance();
+    }
+}
