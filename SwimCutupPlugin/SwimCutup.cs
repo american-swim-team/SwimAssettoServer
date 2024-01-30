34c2c35dc361c99590c16f2ed5a4588841b92323
Rom3dius <rom3dius@yahoo.com>
2024-01-30 14:52:01 +0100

    Added driving stats and better keeping track of highscore

--- SwimCutupPlugin/SwimCutup.cs
+++ SwimCutupPlugin/SwimCutup.cs
@@ -13,57 +13,73 @@ namespace SwimCutupPlugin;
 
 public class SwimCutupPlugin
 {
-    private readonly SwimCutupConfiguration _config;
+    public readonly SwimCutupConfiguration config;
     private readonly CSPClientMessageTypeManager _cspClientMessageTypeManager;
+    private readonly EntryCarManager _entryCarManager;
     private readonly ACServerConfiguration _acServerConfiguration;
-    private readonly HttpClient _http = new HttpClient();
-    private readonly string _trackName;
+    public readonly HttpClient http = new HttpClient();
+    public readonly string _trackName;
+    private Dictionary<ulong, SwimMetricsTracker> _sessions = new();
 
-
-    public SwimCutupPlugin(SwimCutupConfiguration configuration, CSPClientMessageTypeManager cspClientMessageTypeManager, ACServerConfiguration acServerConfiguration)
+    public SwimCutupPlugin(SwimCutupConfiguration configuration, CSPClientMessageTypeManager cspClientMessageTypeManager, EntryCarManager entryCarManager, ACServerConfiguration acServerConfiguration)
     {
         Log.Information("------------------------------------");
         Log.Information("SwimCutupPlugin");
         Log.Information("By Romedius");
         Log.Information("------------------------------------");
 
-        _config = configuration;
+        config = configuration;
         _cspClientMessageTypeManager = cspClientMessageTypeManager;
         _acServerConfiguration = acServerConfiguration;
+
+        _entryCarManager = entryCarManager;
+        _entryCarManager.ClientConnected += OnClientConnected;
+        _entryCarManager.ClientDisconnected += OnClientDisconnected;
 
         _cspClientMessageTypeManager.RegisterClientMessageType(0xBDEC992D, new Action<ACTcpClient, PacketReader>(IncomingCSPEvent));
 
         _trackName = _acServerConfiguration.Server.Track.Substring(_acServerConfiguration.Server.Track.LastIndexOf('/') + 1);
-        Log.Information("SwimCutupPlugin: Track: {trackName}", _trackName);
+
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        timer.Elapsed += new System.Timers.ElapsedEventHandler(UpdateClientSessions);
+        timer.Start();
+    }
+
+    private void UpdateClientSessions(object? sender, EventArgs e)
+    {
+        foreach (var session in _sessions)
+        {
+            ACTcpClient client = session.Value._client;
+            if (client.IsConnected && client.HasSentFirstUpdate) {
+                session.Value.UpdateDriverStats(client.EntryCar.Status.Velocity);
+            }   
+        }
+    }
+
+    private void OnClientConnected(ACTcpClient client, EventArgs args)
+    {
+        _sessions[client.Guid] = new SwimMetricsTracker(this, client, _trackName);
+    }
+
+    private void OnClientDisconnected(ACTcpClient client, EventArgs args)
+    {
+        _sessions[client.Guid].Dispose();
+        _sessions.Remove(client.Guid);
     }
 
     private void IncomingCSPEvent(ACTcpClient client, PacketReader reader)
     {
         long msgType = reader.Read<long>();
         long payload = reader.Read<long>();
-        Log.Information("SwimCutupPlugin: IncomingCSPEvent: msgType: {msgType}, payload: {payload}", msgType, payload);
         switch (msgType)
         {
             case 1:
                 // get highscore from API
-                var highscore_request = new StringContent(JsonSerializer.Serialize(new { steamid = client.Guid, track = _trackName, car = client.EntryCar.Model }), Encoding.UTF8, "application/json");
-                var highscore_response = _http.PostAsync(_config.Server + "/fetch_cutup_score", highscore_request).Result.Content.ReadAsStringAsync().Result;
-                var highscore =  JsonSerializer.Deserialize<Dictionary<string, long>>(highscore_response);
-                client.SendPacket(new SwimCutupMsg { MsgType = 1, Payload = highscore["data"] });
+                _sessions[client.Guid].OnHighscoreRequest();
                 break;
             case 2:
                 // update highscore
-                var new_highscore_request = new StringContent(JsonSerializer.Serialize(new { steamid = client.Guid, track = _trackName, car = client.EntryCar.Model, score = payload }), Encoding.UTF8, "application/json");
-                var new_highscore_response = _http.PostAsync(_config.Server + "/insert_cutup_score", new_highscore_request).Result.Content.ReadAsStringAsync().Result;
-                var status =  JsonSerializer.Deserialize<Dictionary<string, string>>(new_highscore_response);
-                if (status["status"] == "ERROR") {
-                    Log.Information("SwimCutupPlugin: Error updating highscore: {status}", status["message"]);
-                }
-                // fetch new highscore
-                var highscore_update_request = new StringContent(JsonSerializer.Serialize(new { steamid = client.Guid, track = _trackName, car = client.EntryCar.Model }), Encoding.UTF8, "application/json");
-                var highscore_update_response = _http.PostAsync(_config.Server + "/fetch_cutup_score", highscore_update_request).Result.Content.ReadAsStringAsync().Result;
-                var highscore_update =  JsonSerializer.Deserialize<Dictionary<string, long>>(highscore_update_response);
-                client.SendPacket(new SwimCutupMsg { MsgType = 1, Payload = highscore_update["data"] });
+                _sessions[client.Guid].OnCollision(payload);
                 break;
             default:
                 Log.Information("SwimCutupPlugin: Unknown");
